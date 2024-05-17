406289bec6e00992607c1c70a14f4ed720d4e63f
Mansi Rai <139311034+Mansi8588@users.noreply.github.com>
2024-05-17 20:20:42 +0000

    variable type

---
+++ 01_basic/variable.js
@@ -0,0 +1,17 @@
+const accountId=12312
+let accountEmail="mans@gmail.com"
+var accountPawword="ampmfamily"
+accountCity="Deoria"  // possible but pls do not use
+
+let accountStatue  // undefine
+
+// accountId=13 not allowed
+/*not use var because
+ of issue in block scope and functional scope
+*/
+accountEmail="sbde@gmail.com"
+accountPawword="naso"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPawword,accountStatue])
