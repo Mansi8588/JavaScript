ff7efff00489057aba45be3ddecf7416a9688445
Mansi Rai <139311034+Mansi8588@users.noreply.github.com>
2024-05-18 09:55:59 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treate all js code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+
+let nams="mansi"
+let age=21
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigint
+// string=>""
+// boolean =>true/false
+//undefined =>
+    // null => standalone value
+// symbole => to define uniqueness
+
+// object
+
+
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// undefine is self a type but null is object type
