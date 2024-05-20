b7d25bb1d66b48d62ba6fb522e2062c605b23658
Mansi Rai <139311034+Mansi8588@users.noreply.github.com>
2024-05-20 02:54:16 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,46 @@
+ const name="snd"
+ const repo=32
+
+//  console.log(name+repo+"value")
+
+console.log(`Hello mu name is ${name} and my repo count is ${repo} `)
+
+const gameName=new String('jabdgd')
+
+console.log(gameName[0])
+console.log(gameName.__proto__);  // object
+
+
+console.log(gameName.length);  // object
+
+console.log(gameName.toUpperCase());  // object
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf('j'))
+
+const NEWgame=gameName.substring(0,4);
+const NEWgame2=gameName.slice(-5,4);
+console.log(NEWgame)
+console.log(NEWgame2)
+
+
+const nest="     kndmasnod    "
+console.log(nest)
+console.log(nest.trim())
+
+const url="https://mansi.com/mansi%20rai"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('mansi'))
+
+console.log(url.split('/'))
+
+
+
+
+
+
+
+
+
+
