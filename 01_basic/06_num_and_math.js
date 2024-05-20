37cdbf0b52a1c35167fd2b90b7faac0fdb1b5f47
Mansi Rai <139311034+Mansi8588@users.noreply.github.com>
2024-05-20 05:55:31 +0000

    math

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,45 @@
+ const score=3223
+ console.log(score)
+
+ const bal=new Number(2111)
+ console.log(bal)
+
+ console.log(bal.toString().length);
+ console.log(bal.toFixed(2))  // no of points after decimal
+
+
+ const other=1232.12134
+ console.log(other.toPrecision(4)) // return string
+ console.log(other.toPrecision(3))
+
+ const hand=100000
+ console.log(hand.toLocaleString('en-IN'));
+
+
+
+
+ // ********************** Math *********************** //
+
+
+ console.log(Math)
+ console.log(Math.abs(-1))
+ console.log(Math.round(3.2))
+ console.log(Math.ceil(3.2))
+ console.log(Math.floor(3.2))
+ console.log(Math.max(3,2,22,63))
+ console.log(Math.min(3,2,22,63))
+
+ console.log(Math.random())   // bteween 0 to 1
+ console.log(Math.random()*10+1);
+
+
+
+ 
+
+  
+
+
+
+
+
+
